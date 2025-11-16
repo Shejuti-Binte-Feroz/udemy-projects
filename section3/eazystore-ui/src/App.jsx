3d86242dc84aff0efcac6f5b4779321af4214b9f
Shejuti <shejuti.feroz@tigerit.com>
2025-11-16 16:19:20 +0600

    update

--- section3/eazystore-ui/src/App.jsx
+++ section3/eazystore-ui/src/App.jsx
@@ -1,15 +1,17 @@
-import Header from "./components/Header.jsx";
-import Footer from "./components/footer/Footer.jsx";
+import Header from "./components/Header";
+import ClassHeader from "./components/ClassHeader";
+import Footer from "./components/footer/Footer";
 import React from "react";
-import Home from "./components/Home.jsx";
+import Home from "./components/Home";
 
 function App() {
-    return (
-        <React.Fragment>
-            <Header />
-            <Home />
-            <Footer />
-        </React.Fragment>
-    );
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
 }
-export default App
+
+export default App;
