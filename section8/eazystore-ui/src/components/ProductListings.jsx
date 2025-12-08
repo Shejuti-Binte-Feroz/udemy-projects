43162c5b4460c140f05895af278cd76fbb17bbc8
Shejuti <shejuti.feroz@tigerit.com>
2025-12-08 16:33:35 +0600

    update

--- section8/eazystore-ui/src/components/ProductListings.jsx
+++ section8/eazystore-ui/src/components/ProductListings.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import Dropdown from "./Dropdown";
+import SearchBox from "./SearchBox";
+
+const sortList = [
+  "Price: Low to High",
+  "Price: High to Low",
+  "Popularity",
+];
 
 export default function ProductListings({ products }) {
+
+  function handleSearchChange(inputSearch) {
+    console.log(inputSearch);
+  }
+
   return (
     <div className="max-w-6xl mx-auto">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
+        <SearchBox label="Search" placeholder="Search Products..." value="" handleSearch={(value)=>handleSearchChange(value)}></SearchBox>
+        <Dropdown label="Sort By" options={sortList} value="Popularity"></Dropdown>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
-        {products.length > 0 ? (
+        {Array.isArray(products) && products.length > 0 ? (
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))
