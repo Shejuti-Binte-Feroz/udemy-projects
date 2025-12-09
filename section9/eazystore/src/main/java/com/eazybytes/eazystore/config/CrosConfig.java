171a3724474baf4d5507afebdcfd90d9357063a0
Shejuti <shejuti.feroz@tigerit.com>
2025-12-09 16:14:43 +0600

    update

---
+++ section9/eazystore/src/main/java/com/eazybytes/eazystore/config/CrosConfig.java
@@ -0,0 +1,27 @@
+package com.eazybytes.eazystore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+@Configuration
+public class CrosConfig {
+
+    @Bean
+    public CorsFilter crossFilter()
+    {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.addAllowedHeader("Content-Type");
+        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
