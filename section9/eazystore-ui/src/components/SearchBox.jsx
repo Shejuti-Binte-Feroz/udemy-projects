a26c539acb5a27aaa4ee85c2a12756ee9dc354af
Shejuti <shejuti.feroz@tigerit.com>
2025-12-10 11:19:24 +0600

    dark theme applied

--- section9/eazystore-ui/src/components/SearchBox.jsx
+++ section9/eazystore-ui/src/components/SearchBox.jsx
@@ -5,11 +5,11 @@ export default function SearchBox({ label, placeholder, value, handleSearch }) {
   
 
   return (
-    <div className="flex items-center gap-3 pl-4 flex-1 font-primary">
-      <label className="text-lg font-semibold text-primary">{label}</label>
+    <div className="flex items-center gap-3 pl-4 flex-1 font-primary dark:text-light">
+      <label className="text-lg font-semibold text-primary dark:text-light">{label}</label>
       <input
         type="text"
-        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
+        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none dark:focus:ring-lighter text-gray-800 dark:border-light dark:text-lighter"
         placeholder={placeholder}
         value={value}
         onChange={(event)=>handleSearch(event.target.value)}
