bb73a183d1c7fcfbdd5f77ef7ea8a4edbbdb6038
Shejuti <shejutibinteferoz@gmail.com>
2026-01-03 21:37:17 +0600

    update

--- section10/eazystore-ui/src/components/Home.jsx
+++ section10/eazystore-ui/src/components/Home.jsx
@@ -1,54 +1,13 @@
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 import apiClient from "../api/apiClient";
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
-// Hook
+// Hooks
 export default function Home() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // Run once when the component mounts
-  // Mounting is the process of creating and adding the component to the DOM
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const fetchProducts = async () => {
-    try {
-      setLoading(true);
-      const response = await apiClient.get("products");
-      console.log("API response:", response.data); // ✅ log the actual data
-      setProducts(response.data); // update state
-    } catch (error) {
-      setError(
-        error.response?.data?.message ||
-          "An error occurred while fetching products."
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold">Loading products...</span>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold">Error: {error}</span>
-      </div>
-    );
-  }
-
+  const products = useLoaderData();
   return (
-    <div className="max-w-6xl mx-auto px-6 py-8">
+    <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers. Perfect for any occasion!
@@ -56,4 +15,16 @@ export default function Home() {
       <ProductListings products={products} />
     </div>
   );
+}
+
+export async function productsLoader() {
+  try {
+    const response = await apiClient.get("/products"); // Axios GET Request
+    return response.data;
+  } catch (error) {
+    throw new Response(
+      error.message || "Failed to fetch products. Please try again.",
+      { status: error.status || 500 }
+    );
+  }
 }
