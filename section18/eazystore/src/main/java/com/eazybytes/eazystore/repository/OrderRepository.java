6b63fcd346b3cf509e6421316638af13680829bb
Shejuti <shejuti.feroz@tigerit.com>
2026-05-10 15:52:41 +0600

    update

--- section18/eazystore/src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
+++ section18/eazystore/src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
@@ -4,6 +4,8 @@ import com.eazybytes.eazystore.entity.Contact;
 import com.eazybytes.eazystore.entity.Customer;
 import com.eazybytes.eazystore.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +17,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomerOrderByCreatedAtDesc(Customer customer);
 
     List<Order> findByOrderStatus(String orderStatus);
+
+    @Query("SELECT o FROM Order o WHERE o.customer=:customer ORDER BY o.createdAt DESC")
+    List<Order> findOedersByCustomer(@Param("customer") Customer customer);
+
+    @Query("SELECT o FROM Order o WHERE o.orderStatus=?1")
+    List<Order> findOrdersByStatus(String orderStatus);
 }
